65e4192a8958c953e9564ba67a63cfcf0cfba8fb
yogapriya_Shanmugam <yogapriyashanmgam1@gmail.com>
2024-06-05 15:52:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import {useRef, useState} from "react";
 
+const Api_Key = "e8e366c96848b6dea33ee077320fd3d6";
 function App() {
+  const inputRef = useRef(null);
+  const [apiData, setApiData] = useState(null);
+  const [showWeather, setshowWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const WeatherTypes =[
+    {
+      type:"Clear",
+      img:"https://cdn-icons-png.flaticon.com/512/6974/6974833.png",
+    },
+    {
+      type:"Rain",
+      img:"https://cdn-icons-png.flaticon.com/512/3351/3351979.png",
+    },
+    {
+      type:"Snow",
+      img:"https://cdn-icons-png.flaticon.com/512/642/642102.png",
+    },
+    {
+      type:"Haze",
+      img:"https://cdn-icons-png.flaticon.com/512/1197/1197102.png",
+    },
+    {
+      type:"Smoke",
+      img:"https://cdn-icons-png.flaticon.com/512/4380/4380458.png",
+    },
+    {
+      type:"Mist",
+      img:"https://cdn-icons-png.flaticon.com/512/4005/4005901.png",
+    },
+    {
+      type:"Drizzle",
+      img:"https://cdn-icons-png.flaticon.com/512/3076/3076129.png",
+    },
+    {
+      type:"Clouds",
+      img:"https://cdn-icons-png.flaticon.com/512/414/414825.png",
+    }
+  ]
+  const fetchWeather = async()=>{
+    const URL =`https://api.openweathermap.org/data/2.5/weather?q=${inputRef.current.value}&units=metric&appid=${Api_Key}`;
+    setLoading(true);
+    fetch(URL)
+    .then((res)=>res.json())
+    .then((data)=>{
+      setApiData(null);
+      if(data.cod == 404 || data.cod == 400){
+        setshowWeather([
+          {
+            type:"Not Found",
+            img:"https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
+            
+          },
+        ])
+      }
+      setshowWeather(
+        WeatherTypes.filter(
+          (weather) => weather.type === data.weather[0].main
+        )
+      );
+      setApiData(data);
+      setLoading(false);
+    })
+    .catch((err)=>{
+      console.log(err);
+      setLoading(false);
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-800 h-screen grid place-items-center">
+      <div className="bg-white w-96 p-4 rounded-md">
+        <div className="flex items-center justify-between"> 
+          <input type="text" ref={inputRef}
+            placeholder = "Enter Your Location"
+            className="text-xl border-b
+            p-1 border-gray-200 font-semibold uppercase"
+          />
+          <button onClick={fetchWeather}>
+            <img src="https://cdn-icons-png.flaticon.com/512/758/758651.png" alt="..."
+            className="w-8" />
+          </button>
+        </div>
+        <div className={`duration-300 delay-75 overflow-hidden
+        ${showWeather ? "h-[27rem]" : "h-0"}`}>
+          {
+            loading ? (<div className="grid place-items-center h-full">
+              <img className="w-14 mx-auto mb-2 animate-spin" src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png" alt="..." />
+            </div>):(
+              showWeather && (
+                <div className="text-center flex flex-col gap-6 mt-10">
+                  {
+                      apiData && <p className="text-xl font-semibold">{apiData?.name + ',' + apiData?.sys?.country}</p>
+                  }
+                
+            <img src={showWeather[0]?.img} alt="..." className="w-52 mx-auto" />
+            <h3 className="text-2xl font-bold text-zinc-800">{showWeather[0]?.type}</h3>
+           {
+            apiData && ( <>
+              <div className="flex justify-center">
+                <img className="h-9 mt-1" src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" />
+                <h2 className="text-4xl font-extrabold">{apiData?.main?.temp}&#176;C</h2>
+              </div>
+              </>)
+           }
+            </div>
+              )
+            )
+          }
+        </div>
+      </div>
     </div>
+    
   );
 }
 
